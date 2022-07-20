cc64af484dfc89060f845050b646739dbfb40f46
Dioner Weiss <dionerweiss@gmail.com>
2022-07-19 23:12:49 -0300

    feat: create customer entity

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,11 +1,50 @@
-class Customer {
-  _id: string;
-  _name: string;
-  _address: string;
+import Address from "../value-object/address";
 
-  constructor(id: string, name: string, address: string) {
+export class Customer {
+  private _id: string;
+  private _name: string;
+  private _address!: Address;
+  private _active: boolean = true;
+
+  constructor(id: string, name: string) {
     this._id = id;
     this._name = name;
+    this.validate();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  validate() {
+    if (this._id.length === 0) {
+      throw new Error('Id is required');
+    } else if (this._name.length === 0) {
+      throw new Error('Name is required');
+    }
+  }
+
+  changeName(name: string) {
+    this._name = name;
+    this.validate();
+  }
+
+  set Address(address: Address) {
     this._address = address;
+  }
+
+  activate() {
+    if(this._address === undefined) {
+      throw new Error('Address is mandatory to activate customer');
+    }
+    this._active = true;
+  }
+
+  deactivate() {
+    this._active = false;
+  }
+
+  isActive() {
+    return this._active;
   }
 }
