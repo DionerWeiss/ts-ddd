b420eaac3f4773b820dfdf0c4caf369cc2b2d596
Dioner Weiss <dionerweiss@gmail.com>
2022-07-21 00:02:43 -0300

    test: validate item quantity

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -23,7 +23,7 @@ describe('Order unit tests', () => {
     expect(() => {
       new Order
       ('123', '123', []);
-    }).toThrowError('Item quantity must be greater than 0');
+    }).toThrowError('Items are required');
   })
 
   it('should calculate total', () => {
@@ -40,6 +40,14 @@ describe('Order unit tests', () => {
     total = order2.total();
 
     expect(total).toBe(300);
+  })
+
+  it('should throw error if the item quantity is less or equal zero', () => {
+    expect(() => {
+      const item = new OrderItem("i1", "Item 1", 100, "p1", 0)
+      const order = new Order("o1", "c1", [item])
+    }).toThrowError("Item quantity must be greater than 0")
+
   })
 
 });
