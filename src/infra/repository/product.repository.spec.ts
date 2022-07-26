01904c7c40cd860cfd5c4374e7ba6e908fd0a58b
Dioner Weiss <dionerweiss@gmail.com>
2022-07-25 22:50:14 -0300

    test: should create a product

--- src/infra/repository/product.repository.spec.ts
+++ src/infra/repository/product.repository.spec.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import { Product } from "../../domain/entity/product";
+import { ProductModel } from "../db/sequelize/model/product.model";
+import { ProductRepository } from "./product.repository";
 
 describe('Product repository unit tests', () => {
   
@@ -7,13 +10,31 @@ describe('Product repository unit tests', () => {
   beforeEach(async () => {
     sequelize = new Sequelize({
       dialect: 'sqlite',
-      storage: ':memory',
+      storage: ":memory:",
       logging: false,
       sync: {force: true}
     })
+
+     sequelize.addModels([ProductModel])
+     await sequelize.sync()
   })
 
   afterEach(async () => {
     await sequelize.close()
+  })
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository()
+    const product = new Product("1", "product 1", 100)
+
+    await productRepository.create(product)
+
+    const productModel = await ProductModel.findOne({where: {id: "1"}})
+
+    expect(productModel?.toJSON()).toStrictEqual({
+      id: "1",
+      name: "product 1",
+      price: 100
+    })
   })
 });
