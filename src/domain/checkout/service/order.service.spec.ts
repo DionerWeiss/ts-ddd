5d269cea2ddcba1ffc5f0d0775413fcde048ee0e
Dioner Weiss <dionerweiss@gmail.com>
2022-08-03 23:34:27 -0300

    refactor: move customer aggregate to its own module

--- src/domain/checkout/service/order.service.spec.ts
+++ src/domain/checkout/service/order.service.spec.ts
@@ -1,6 +1,6 @@
 import { Order } from "../entity/order";
 import { OrderItem } from "../entity/order-item";
-import { Customer } from "../../entity/customer";
+import { Customer } from "../../customer/entity/customer";
 import { OrderService } from "./order.service";
 
 describe('Order service unit tests', () => {
