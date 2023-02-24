cd219ec0f50e9945ba9842b496ef22afd130879a
dioner <dionerweiss@gmail.com>
2023-02-23 23:25:00 -0300

    feat: list customers usecase

---
+++ src/usecase/customer/list/list.customer.usecase.ts
@@ -0,0 +1,30 @@
+import { Customer } from "../../../domain/customer/entity/customer";
+import { CustomerRepositoryInterface } from "../../../domain/customer/repository/customer-repository.interface";
+import { InputListCustomerDto, OutputListCustomerDto } from "./list.customer.dto";
+
+export class ListCustomerUseCase {
+  constructor(private readonly customerRepository: CustomerRepositoryInterface){}
+
+  async execute(input: InputListCustomerDto): Promise<OutputListCustomerDto> {
+    const customers = await this.customerRepository.findAll()
+
+    return OutputMapper.toOutput(customers)
+  }
+}
+
+class OutputMapper {
+  static toOutput(customers: Customer[]): OutputListCustomerDto {
+    return {
+      customers: customers.map(customer => ({
+        id: customer.id,
+        name: customer.name,
+        address: {
+          street: customer.Address.street,
+          city: customer.Address.city,
+          number: customer.Address.number,
+          zip: customer.Address.zip
+        }
+      }))
+    }
+  }
+}
