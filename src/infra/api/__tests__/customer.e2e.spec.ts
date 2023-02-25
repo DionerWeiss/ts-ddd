adccc5b3cfc98a0969c0f83d2409d70745a43005
dioner <dionerweiss@gmail.com>
2023-02-24 23:14:52 -0300

    test: list all customer route

--- src/infra/api/__tests__/customer.e2e.spec.ts
+++ src/infra/api/__tests__/customer.e2e.spec.ts
@@ -33,4 +33,54 @@ describe('E2E test for costumer', () => {
       expect(response.body.address.number).toBe(123)
       expect(response.body.address.zip).toBe("123123")
   });
+
+  it('should not create a customer', async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+       
+      })
+
+    expect(response.status).toBe(500)
+  });
+
+  it('should list all customers', async () => {
+    const response = await request(app)
+    .post("/customer")
+    .send({
+      name: "John",
+      address: {
+        street: "Street",
+        city: "City",
+        number: 123,
+        zip: "123123"
+      }
+    })
+    expect(response.status).toBe(200)
+    const response2 = await request(app)
+    .post("/customer")
+    .send({
+      name: "Jane",
+      address: {
+        street: "Street 2",
+        city: "City 2",
+        number: 456,
+        zip: "456456"
+      }
+    })
+    expect(response.status).toBe(200)
+
+    const listResponse = await request(app)
+      .get("/customer")
+      .send()
+
+    expect(listResponse.status).toBe(200)
+    expect(listResponse.body.customers.length).toBe(2)
+    const customer1 = listResponse.body.customers[0]
+    expect(customer1.name).toBe("John")
+    expect(customer1.address.street).toBe("Street")
+    const customer2 = listResponse.body.customers[1]
+    expect(customer2.name).toBe("Jane")
+    expect(customer2.address.street).toBe("Street 2")
+  });
 });
